f6fa39f70f6a86b8b8836ed57a624c62a95fd787
Philipp Schopp <philipp.schopp@iav.de>
2025-12-02 12:40:06 +0100

    first commit

---
+++ 07/main.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import re
+from tqdm import tqdm
+from helpers.get_input import get_lines
+
+
+def do_main(debug_mode=False):
+    lines = get_lines('07')
+
+    point_sum = 0
+
+    for line_index, line in enumerate(lines):
+        r = [int(i) for i in line.split(" ")]
+
+if __name__ == '__main__':
+    do_main(False)
