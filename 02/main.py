6acf7a3bb407bc9789692b20cf87f44c1f423b1c
Philipp Schopp <philipp.schopp@iav.de>
2025-12-03 08:22:30 +0100

    added 2,3

--- 02/main.py
+++ 02/main.py
@@ -1,16 +1,30 @@
-from pathlib import Path
-import re
-from tqdm import tqdm
+from collections import Counter
 from helpers.get_input import get_lines
 
 
 def do_main(debug_mode=False):
     lines = get_lines('02', debug_mode)
 
-    point_sum = 0
+    point_sum_2 = 0
+    point_sum_3 = 0
 
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        counts = Counter(line)
+        if 2 in counts.values():
+            point_sum_2 += 1
+        if 3 in counts.values():
+            point_sum_3 += 1
+
+        for line2 in lines[line_index:]:
+            dist = 0
+            last_bad = 0
+            for i, c in enumerate(line):
+                if c != line2[i]:
+                    dist+=1
+                    last_bad = i
+            if dist == 1:
+                print(line[:last_bad]+line[last_bad+1:])
+    print(point_sum_2*point_sum_3)
 
 if __name__ == '__main__':
     do_main(False)
--- 03/main.py
+++ 03/main.py
@@ -1,16 +1,34 @@
-from pathlib import Path
 import re
-from tqdm import tqdm
+
+import numpy as np
 from helpers.get_input import get_lines
 
 
 def do_main(debug_mode=False):
     lines = get_lines('03', debug_mode)
 
-    point_sum = 0
+    grid = np.zeros((1000, 1000))
+    overlapping = set()
+    overlapped_fields = set()
+    ids = set()
 
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        id, lm, tm, width, height = [int(x) for x in re.findall(r'\d+', line)]
+        ids.add(id)
+        for i in range(lm, lm + width):
+            for j in range(tm, tm + height):
+                if grid[j,i] > 0:
+                    overlapping.add(grid[j,i])
+                    overlapping.add(id)
+                    overlapped_fields.add((i,j))
+                grid[j,i] = id
+
+    print(len(overlapped_fields))
+
+    for id in ids:
+        if id not in overlapping:
+            print(id)
+            break
 
 if __name__ == '__main__':
     do_main(False)
